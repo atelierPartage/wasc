a299ac0e2a2ed5e1df7d57139994d55b753bad88
Guillaume Collet <bilouweb@free.fr>
2023-07-03 13:54:21 +0200

    some typos in clio

--- src/wasc/cli/__init__.py
+++ src/wasc/cli/__init__.py
@@ -14,7 +14,10 @@ from wasc.report import Report
 from wasc.utils import dict_to_csv, read_criteria_config, read_websites
 
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
-DEFAULT_CRIT_DICT = { "Accessibilité" : ["AccessChecker", "AccessLinkChecker"], "Mentions légales" : ["MentionsLegalesChecker"]}
+DEFAULT_CRIT_DICT = {
+    "Accessibilité" : ["AccessChecker", "AccessLinkChecker"],
+    "Mentions légales" : ["MentionsLegalesChecker"]
+    }
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument("websites", type=click.Path(exists=True))
